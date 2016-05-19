c01e4957df8659ed96b2715329d3b722b7f9eaca
Edward Knyshov <edvorg@gmail.com>
2016-05-20 03:44:18 +0700

    use ensime scala mode

--- init.d/init-scala.el
+++ init.d/init-scala.el
@@ -1,6 +1,6 @@
 (require 'req-package)
 
-(req-package scala-mode2
+(req-package scala-mode
   :config (progn (setq scala-indent:align-parameters t))
   :mode (("\\.scala\\'" . scala-mode)
          ("\\.sc\\'" . scala-mode)))
@@ -8,8 +8,8 @@
 (req-package sbt-mode
   :commands sbt-start)
 
-;; (req-package ensime
-;;   :require (scala-mode2 auto-complete)
-;;   :config (req-package-hooks-add-execute 'scala-mode 'ensime-scala-mode-hook))
+(req-package ensime
+  :require (scala-mode auto-complete)
+  :config (req-package-hooks-add-execute 'scala-mode 'ensime-scala-mode-hook))
 
 (provide 'init-scala)
