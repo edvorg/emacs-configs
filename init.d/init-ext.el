99d38669526572769baf178199d628f64a51bca6
Edward Knyshov <edvorg@gmail.com>
2016-05-21 18:10:32 +0700

    tweak elfeed filter

--- init.d/init-ext.el
+++ init.d/init-ext.el
@@ -328,6 +328,7 @@
   (setf url-queue-timeout 60)
   (load "~/Dropbox/emacs/elfeed-feeds.el" t)
   (setq elfeed-db-directory "~/Dropbox/emacs/elfeed")
-  (setq elfeed-enclosure-default-dir "~/Dropbox/emacs"))
+  (setq elfeed-enclosure-default-dir "~/Dropbox/emacs")
+  (setq elfeed-search-filter "@1-months-ago +unread"))
 
 (provide 'init-ext)
