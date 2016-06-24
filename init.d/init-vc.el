b3200ff6a7dd67f7e9618d17884079ce66143817
Edward Knyshov <edvorg@gmail.com>
2016-06-24 21:08:45 +0700

    add directory tree comparison utils

--- init.d/init-vc.el
+++ init.d/init-vc.el
@@ -1,9 +1,5 @@
 (require 'req-package)
 
-;; svn integration
-
-;; in-project search
-
 (defun find-upper-svn-root (from acc)
   (let* ((UPPER (expand-file-name (concat from "/..")))
 		 (NEWACC (cond ((file-exists-p (concat from "/.svn")) from)
@@ -20,20 +16,14 @@
   :commands svn-status
   :chords ((";s" . upper-svn-status)))
 
-;; use timemachine
-
 (req-package git-timemachine
   :bind (("C-x v t" . git-timemachine)))
-
-;; magit
 
 (req-package magit
   :chords ((";m" . magit-status))
   :init
   (setq magit-last-seen-setup-instructions "1.4.0")
   (setq magit-auto-revert-mode t)())
-
-;; git flow
 
 (req-package magit-gitflow
   :require magit
@@ -44,38 +34,25 @@
   :require magit
   :config (add-hook-exec 'magit-mode 'turn-on-magit-gh-pulls))
 
-;; magit svn
-
 (req-package magit-svn :require magit)
-
-;; helm ls git
 
 (req-package helm-ls-git
   :require helm
   :bind ("M-+" . helm-ls-git-ls))
 
-;; git config mode
-
 (req-package gitconfig-mode)
 
-;; git ignore mode
-
 (req-package gitignore-mode)
-
-;; git message
 
 (req-package git-messenger
   :bind ("C-x v p" . git-messenger:popup-message))
 
-;; git blame
-
 (req-package git-blame)
-
-;; git attributes editing
 
 (req-package gitattributes-mode)
 
-;; tweak ediff
+(req-package ztree
+  :bind ("<f9>" . ztree-diff))
 
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 (setq ediff-split-window-function 'split-window-horizontally)
