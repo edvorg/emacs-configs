df74e2288559bc786002c619f334bc28619b15d3
Edward Knyshov <edvorg@gmail.com>
2017-02-16 11:37:42 +0700

    copy variables defined in ~/.bash_profile

--- init.d/init-darwin.el
+++ init.d/init-darwin.el
@@ -9,6 +9,14 @@
 		   (setenv "ANDROID_HOME" "/usr/local/opt/android-sdk")
 		   (setenv "ANDROID_NDK" "/usr/local/opt/android-ndk")
            (add-to-list 'exec-path "/usr/local/bin")
+           (req-package exec-path-from-shell
+             :require dash
+             :init (let* ((variables (shell-command-to-string "cat ~/.bash_profile | grep -e '^export' | sed -e 's/^export \\([A-Za-z0-9_]*\\)=.*$/\\1/g'"))
+                          (variables (split-string variables "\n"))
+                          (variables (-drop-last 1 variables)))
+                     (setq exec-path-from-shell-variables variables))
+             :config (exec-path-from-shell-initialize))
+
            ;; (set-default-font "Hack-12" t)
            ;; (set-default-font "Liberation Mono-12" t)
            ;; (set-default-font "Oxygen Mono-12" t)
