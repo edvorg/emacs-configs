f20b6cb0093591608b87890f70e72e61fd2c21eb
Edward Knyshov <edvorg@gmail.com>
2017-02-16 11:38:20 +0700

    tweak bindings

--- init.d/init-ext.el
+++ init.d/init-ext.el
@@ -329,7 +329,7 @@
 
 (req-package guide-key
   :config
-  (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x C-a" "C-c" "C-x" "C-c C-v"))
+  (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x C-a" "C-c" "C-x" "C-c C-v" "C-c C-t"))
   (guide-key-mode 1))
 
 (req-package easy-kill
