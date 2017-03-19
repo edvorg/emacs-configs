170d3e073c4e0623de21912c1d0e9dbc266c6e6b
Edward Knyshov <edvorg@gmail.com>
2017-03-19 19:11:28 +0700

    fix font setup on linux

--- init.d/init-linux.el
+++ init.d/init-linux.el
@@ -1,5 +1,12 @@
 (require 'req-package)
 
+(defun setup-linux-font (frame)
+  (condition-case-unless-debug e
+      (progn
+        (add-to-list 'default-frame-alist '(font . "Source Code Pro Semibold 11"))
+        (set-default-font "Source Code Pro Semibold 11" t))
+    (error (message "can not load SourceCodePro font : %s" e))))
+
 (when (eq system-type 'gnu/linux)
   (req-package browse-url
     :config (setq browse-url-browser-function
@@ -8,6 +15,8 @@
   (req-package aurel :commands aurel-package-search)
   (req-package helm-systemd
     :require helm
-    :commands helm-systemd))
+    :commands helm-systemd)
+  (setup-linux-font nil)
+  (add-to-list 'after-make-frame-functions 'setup-linux-font))
 
 (provide 'init-linux)
