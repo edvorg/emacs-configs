7e626b1bfa3ff545fab8e142ed3ff1fe1dea80ba
Edward Knyshov <edvorg@gmail.com>
2018-05-04 23:23:36 +0800

    tweak ignored files

--- init.d/init-search.el
+++ init.d/init-search.el
@@ -68,11 +68,12 @@
             (add-to-list 'grep-find-ignored-directories ".release")
             (add-to-list 'grep-find-ignored-directories "xwiki")
             (add-to-list 'grep-find-ignored-directories "target")
-            (add-to-list 'grep-find-ignored-directories "resources")
             (add-to-list 'grep-find-ignored-directories "xwiki-raven")
             (add-to-list 'grep-find-ignored-directories "node_modules")
             (add-to-list 'grep-find-ignored-directories ".sass-cache")
             (add-to-list 'grep-find-ignored-directories "resources/public/js")
+            (add-to-list 'grep-find-ignored-files "package-lock.json")
+            (add-to-list 'grep-find-ignored-files "Cargo.lock")
             (add-hook-exec 'grep-mode (lambda () (toggle-truncate-lines 1)))))
 
 (provide 'init-search)
