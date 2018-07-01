730783c4a1ce5c4d57c3a6d5bf2aa8a1bd16e14b
Edward Knyshov <edvorg@gmail.com>
2018-07-01 21:01:59 +0700

    use lsp in rust mode

--- init.d/init-rust.el
+++ init.d/init-rust.el
@@ -2,33 +2,18 @@
 
 (req-package rust-mode
   :ensure t
-  :pin melpa-stable
   :mode (("rs\\'" . rust-mode))
   :init
   (add-to-list 'exec-path "~/.cargo/bin")
   :config
-  (add-hook-exec 'rust-mode #'racer-mode))
-
-(req-package racer
-  :ensure t
-  :pin melpa-stable
-  :commands (racer-mode)
-  :init
-  (setq racer-rust-src-path (shell-command-to-string "echo -n $(rustc --print sysroot)/lib/rustlib/src/rust/src"))
-  :config
-  (add-hook-exec 'racer-mode #'eldoc-mode)
-  (add-hook-exec 'racer-mode #'company-mode)
-  (add-hook-exec 'racer-mode #'cargo-minor-mode))
+  (add-hook-exec 'rust-mode #'lsp-rust-enable))
 
 (req-package cargo
   :ensure t
-  :pin melpa-stable
   :commands cargo-minor-mode)
 
-(req-package flycheck-rust
-  :ensure t
-  :require flycheck
-  :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+(req-package lsp-rust
+  :commands lsp-rust-enable)
 
 (req-package toml-mode
   :ensure t
