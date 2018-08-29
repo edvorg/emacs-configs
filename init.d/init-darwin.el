754c55c29c62a5b8974640a3c4fd72340c78609a
Eduard Knyshov <edvorg@edvorg-macbook.localdomain>
2018-08-29 10:44:48 +0800

    add osx setup

--- init.d/init-darwin.el
+++ init.d/init-darwin.el
@@ -1,19 +1,21 @@
 (require 'req-package)
 
-(defun setup-linux-font (frame)
+(defun setup-darwin-font (frame)
   (condition-case-unless-debug e
       (progn
         (add-to-list 'default-frame-alist '(font . "Source Code Pro Semibold 9"))
         (set-default-font "Source Code Pro Semibold 9" t))
     (error (message "can not load SourceCodePro font : %s" e))))
 
-(when (eq system-type 'gnu/linux)
+(when (eq system-type 'darwin)
   (req-package browse-url
     :ensure t
     :config (setq browse-url-browser-function
                   (lambda (url &rest args)
-                    (call-process-shell-command "xdg-open" nil 0 nil url))))
-  (setup-linux-font nil)
-  (add-to-list 'after-make-frame-functions 'setup-linux-font))
+                    (call-process-shell-command "open" nil 0 nil url))))
 
-(provide 'init-linux)
+  (thread-last (getenv "PATH")
+    (concat "/usr/local/bin:")
+    (setenv "PATH")))
+
+(provide 'init-darwin)
