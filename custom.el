c0ce5e1341f7ba3afd62d446d19f111fd581868f
Eduard Knyshov <edvorg@gmail.com>
2018-12-31 13:45:39 +0000

    Add custom.el

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (nix-mode zygospore yoshi-theme yaml-mode wgrep visual-regexp-steroids use-package-chords undercover swift-mode string-edit smex smartrep smartparens smart-mode-line shell-pop scss-mode req-package rectangle-utils rainbow-delimiters protobuf-mode projectile paradox overseer org-pomodoro nyan-mode nameless multiple-cursors move-text markdown-mode magit-gitflow lua-mode load-dir kaolin-themes json-mode ido-at-point idle-highlight-mode helm-themes helm-swoop helm-proc helm-github-stars helm-descbinds helm-company helm-books guide-key gruvbox-theme gotham-theme google-translate git-timemachine git-messenger flycheck-pos-tip flx-ido expand-region ensime emmet-mode elisp-slime-nav el-mock dockerfile-mode diff-hl define-word darktooth-theme company-shell company-quickhelp clojure-mode cargo bash-completion anzu ace-window ace-link ace-jump-mode ace-jump-helm-line ace-jump-buffer)))
+ '(rainbow-delimiters-max-face-count 8))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
