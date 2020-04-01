f1fba276f21e877a3a8af2812dbc069e22f915a5
Eduard Knyshov <edvorg@gmail.com>
2020-04-01 10:16:44 +0800

    update custom.el

--- custom.el
+++ custom.el
@@ -3,10 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-fontify-whole-heading-line t)
  '(package-selected-packages
    (quote
-    (yasnippet magit scala-mode rust-mode json-reformat flx helm flycheck el-get use-package nix-mode zygospore yoshi-theme yaml-mode wgrep visual-regexp-steroids use-package-chords undercover swift-mode string-edit smex smartrep smartparens smart-mode-line shell-pop scss-mode req-package rectangle-utils rainbow-delimiters protobuf-mode projectile paradox overseer org-pomodoro nyan-mode nameless multiple-cursors move-text markdown-mode magit-gitflow lua-mode load-dir kaolin-themes json-mode ido-at-point idle-highlight-mode helm-themes helm-swoop helm-proc helm-github-stars helm-descbinds helm-company helm-books guide-key gruvbox-theme gotham-theme google-translate git-timemachine git-messenger flycheck-pos-tip flx-ido expand-region ensime emmet-mode elisp-slime-nav el-mock dockerfile-mode diff-hl define-word darktooth-theme company-shell company-quickhelp clojure-mode cargo bash-completion anzu ace-window ace-link ace-jump-mode ace-jump-helm-line ace-jump-buffer)))
- '(rainbow-delimiters-max-face-count 8))
+    (plan9-theme clj-refactor clojure-snippets cider yasnippet magit scala-mode rust-mode json-reformat flx helm flycheck el-get use-package nix-mode zygospore yoshi-theme yaml-mode wgrep visual-regexp-steroids use-package-chords undercover swift-mode string-edit smex smartrep smartparens smart-mode-line shell-pop scss-mode req-package rectangle-utils rainbow-delimiters protobuf-mode projectile paradox overseer org-pomodoro nyan-mode nameless multiple-cursors move-text markdown-mode magit-gitflow lua-mode load-dir kaolin-themes json-mode ido-at-point idle-highlight-mode helm-themes helm-swoop helm-proc helm-github-stars helm-descbinds helm-company helm-books guide-key gruvbox-theme gotham-theme google-translate git-timemachine git-messenger flycheck-pos-tip flx-ido expand-region ensime emmet-mode elisp-slime-nav el-mock dockerfile-mode diff-hl define-word darktooth-theme company-shell company-quickhelp clojure-mode cargo bash-completion anzu ace-window ace-link ace-jump-mode ace-jump-helm-line ace-jump-buffer)))
+ '(paradox-github-token t)
+ '(rainbow-delimiters-max-face-count 8)
+ '(window-divider-default-right-width 1)
+ '(window-divider-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
